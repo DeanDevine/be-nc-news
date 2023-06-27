467819170f08e73d43624f5ca38497020652198a
DeanDevine <deanrussondevine@gmail.com>
2023-06-27 15:29:48 +0100

    add controller/model for GET /api/articles and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,10 +28,10 @@ describe('GET /api/topics', () => {
     })
     test('status:404, responds with error message "Not Found" when path does not exist', () => {
         return request(app)
-          .get('/api/topic')
-          .expect(404)
-          .then(({ body }) => {
-            expect(body.msg).toBe('Not Found');
+        .get('/api/topic')
+        .expect(404)
+        .then(({ body }) => {
+        expect(body.msg).toBe('Not Found');
           });
       });
 })
@@ -92,3 +92,90 @@ describe('GET /api/articles/:article_id', () => {
         })
     })
 })
+
+describe('GET /api/articles', () => {
+    test('status:200, responds with a JSON object containing an array of article objects, each of which contains the following properties: author, title, article_id, topic, created_at, votes, article_img_url', () => {
+        return request(app)
+        .get('/api/articles')
+        .expect(200)
+        .then(({body}) => {
+
+        const { articles } = body;
+
+        expect(articles).toHaveLength(5);
+
+        articles.forEach((article) => {
+                expect(article).toMatchObject({
+                    author: expect.any(String),
+                    title: expect.any(String),
+                    article_id: expect.any(Number),
+                    topic: expect.any(String),
+                    created_at: expect.any(String),
+                    votes: expect.any(Number),
+                    article_img_url: expect.any(String)
+                    })
+        })
+        })
+    })
+    test('status:200, each article object has a comment_count property which is the total count of all comments with that article_id', () => {
+        return request(app)
+        .get('/api/articles')
+        .expect(200)
+        .then(({ body }) => {
+            
+            const { articles } = body;
+
+            articles.forEach((article) => {
+                expect(article).toHaveProperty("comment_count", expect.any(String));
+
+            })
+        })
+    });
+    test('status:200, article objects do not have a body property', () => {
+        return request(app)
+        .get('/api/articles')
+        .expect(200)
+        .then(({ body }) => {
+            
+            const { articles } = body;
+
+            console.log(articles)
+
+            articles.forEach((article) => {
+                expect(article).not.toHaveProperty("body");
+
+            })
+        })
+    });
+    test('status:200, articles are sorted by their created_at date in descending order', () => {
+        return request(app)
+        .get('/api/articles')
+        .expect(200)
+        .then(({ body }) => {
+            
+            const { articles } = body;
+
+            const oldestArticle = {
+                author: 'icellusedkars',
+                title: 'Eight pug gifs that remind me of mitch',
+                article_id: 3,
+                topic: 'mitch',
+                created_at: '2020-11-03T09:12:00.000Z',
+                votes: 0,
+                article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
+                comment_count: '2'
+              }
+
+            expect(articles[0]).toEqual(oldestArticle)
+
+        })
+    })
+    test('status:404, responds with error message "Not Found" when path does not exist', () => {
+        return request(app)
+        .get('/api/article')
+        .expect(404)
+        .then(({ body }) => {
+        expect(body.msg).toBe('Not Found');
+          });
+      });
+});
