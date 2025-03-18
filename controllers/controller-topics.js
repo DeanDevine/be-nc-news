8ae056b1319e1fbace79023b4a9a9a1d723076e2
DeanDevine <deanrussondevine@gmail.com>
2025-03-18 15:50:43 +0000

    add debugging for fetching topics

--- controllers/controller-topics.js
+++ controllers/controller-topics.js
@@ -1,20 +1,23 @@
-const { selectTopics, insertTopic } = require("../models/model-topics")
+const { selectTopics, insertTopic } = require("../models/model-topics");
 
 exports.getTopics = (req, res, next) => {
-
-    selectTopics().then((topics) => {
-    res.status(200).send({topics})
-
-})
-.catch(next);
-}
+  selectTopics()
+    .then((topics) => {
+      console.log("Found topics:", topics); // debugging
+      res.status(200).send({ topics });
+    })
+    .catch((err) => {
+      console.log("Error fetching topics:", err); // debugging
+      next(err);
+    });
+};
 
 exports.postTopic = (req, res, next) => {
+  const { slug, description } = req.body;
 
-    const { slug, description } = req.body;
-
-    insertTopic(slug, description).then((topic) => {
-        res.status(201).send({ topic })
+  insertTopic(slug, description)
+    .then((topic) => {
+      res.status(201).send({ topic });
     })
-    .catch(next)
-}
+    .catch(next);
+};
